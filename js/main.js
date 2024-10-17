7c6a504b05312c4d793c4c46b0b8861b37cc3d87
Gargenziano03 <gargenziano03@gmail.com>
2024-10-17 16:35:25 +0200

    add reasoning

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 //Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail 
 //generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 console.log('hello');
+
+//fase 1: richiamare l'id ad HTML (listEmail)
+//fase 2: creare un array raccogliere l'email
+//fase 3: funzione per generare le email
+//fase 4: ciclo for per aggiungere l'amail all'array
+//fase 5: stamapare l'email in pagina
+
