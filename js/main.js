3de1b475865754863993ac74b4659bcd3d4c22b6
Gargenziano03 <gargenziano03@gmail.com>
2024-10-17 17:16:44 +0200

    add  richiesta all'api

--- js/main.js
+++ js/main.js
@@ -7,6 +7,19 @@ const listEmailEl = document.getElementById('listEmail');
 //fase 2: creare un array raccogliere l'email
 const emails = [];
 //fase 3: funzione per generare le email
+const randomEmails =  async () => {
 //fase 4: ciclo for per aggiungere l'amail all'array
+for (let i = 0; i < 10; i++) {
+    try{
+        //richiesta all'API
+        const response = await axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        //aggiunge l'indirizzo nel array
+        emails.push(response.data.response);
+    }
+    
+}
+    
+}
+
 //fase 5: stamapare l'email in pagina
 
